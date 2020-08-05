a8bc14330ae68fbb7b4ce7589ff4e0c39c4cfcfd
Baptiste Rozière <broz@fb.com>
2020-08-05 11:49:03 +0200

    Corrected the ground truth in MINIMUM_ROTATIONS_REQUIRED_GET_STRING tests

--- data/evaluation/geeks_for_geeks_successful_test_scripts/python/MINIMUM_ROTATIONS_REQUIRED_GET_STRING.py
+++ data/evaluation/geeks_for_geeks_successful_test_scripts/python/MINIMUM_ROTATIONS_REQUIRED_GET_STRING.py
@@ -8,7 +8,7 @@ def f_gold ( str ) :
     tmp = str + str
     n = len ( str )
     for i in range ( 1 , n + 1 ) :
-        substring = tmp [ i : n ]
+        substring = tmp [ i : i + n ]
         if ( str == substring ) :
             return i
     return n
